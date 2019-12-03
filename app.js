fa915f44f608a1be316eae2497ee4dd2033bd6e0
jean-smaug <maximeblanc.dev@gmail.com>
2019-12-03 13:19:01 +0100

    style: format js files

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const evtSource = new EventSource('http://localhost:5000');
 const seriesData = []
 
 const options = {
-    chart: {
-        type: 'area'
-    },
-    series: [{
-        name: 'area',
-        data: []
-    }],
-    xaxis: {
-        categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-    }
+  chart: {
+    type: 'area'
+  },
+  series: [{
+    name: 'area',
+    data: []
+  }],
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+  }
 }
 
 const chart = new ApexCharts(document.getElementById('chart'), options);
@@ -20,19 +20,18 @@ const chart = new ApexCharts(document.getElementById('chart'), options);
 chart.render();
 
 evtSource.onopen = function () {
-    console.log("OPENED")
+  console.log("OPENED")
 }
 
-evtSource.onmessage = function(e) {
-    console.log("new message")
-    seriesData.push(JSON.parse(e.data).time)
-    setTimeout(() => {
-        chart.updateSeries([{
-            data: seriesData
-        }])
-    }, 2000)
+evtSource.onmessage = function (e) {
+  seriesData.push(JSON.parse(e.data).time)
+  setTimeout(() => {
+    chart.updateSeries([{
+      data: seriesData
+    }])
+  }, 2000)
 }
 
-evtSource.onerror = function(e) {
-    console.log('merde', e)
+evtSource.onerror = function (e) {
+  console.log('merde', e)
 }
--- server.js
+++ server.js
@@ -1,48 +1,46 @@
 // https://stackoverflow.com/questions/53888030/why-do-server-sent-events-initiate-every-3-seconds
 const PORT = 5000
-const Koa = require("koa")
-const { PassThrough } = require("stream")
+// const Koa = require("koa")
+// const { PassThrough } = require("stream")
 
-const app = new Koa()
+// const app = new Koa()
 
-app.use(async (ctx) => {
-    ctx.set({
-        "Access-Control-Allow-Origin": "*",
-        Connection: "keep-alive",
-        'Cache-Control': 'no-cache'
-    })
-    
-    const stream = new PassThrough()
-    
-    setInterval(() => {
-        const data = { "time": Math.round(Math.random() * 1000) }
-        stream.write(`data: ${JSON.stringify(data)}\n\n`)
-    }, 200)
-    
-    ctx.type = "text/event-stream";
-    ctx.body = stream
-});
-
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
-
-// const http = require("http")
-
-// http.createServer((req, res) => {
-//     res.writeHead(200, {
-//         "Content-Type": "text/event-stream",
+// app.use(async (ctx) => {
+//     ctx.set({
 //         "Access-Control-Allow-Origin": "*",
-//         "Connection": "keep-alive",
+//         Connection: "keep-alive",
 //         'Cache-Control': 'no-cache'
 //     })
 
-//     setInterval(() => {
-//         const data = { "time": Math.round(Math.random() * 100) }
-//         res.write(`data: ${JSON.stringify(data)}`)
-//         res.write("\n\n")
-//     }, 1000)
+//     const stream = new PassThrough()
 
-//         const data = { "time": Math.round(Math.random() * 100) }
-//         res.write(`data: ${JSON.stringify(data)}`)
-//         res.write("\n\n")
-//     })
-//     .listen(PORT, () => console.log(`http://localhost:${PORT}`))
+//     setInterval(() => {
+//         const data = { "time": Math.round(Math.random() * 1000) }
+//         stream.write(`data: ${JSON.stringify(data)}\n\n`)
+//     }, 200)
+
+//     ctx.type = "text/event-stream";
+//     ctx.body = stream
+// });
+
+// app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
+
+const http = require("http")
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+      "Access-Control-Allow-Origin": "*",
+      "Connection": "keep-alive",
+      'Cache-Control': 'no-cache'
+    })
+
+    setInterval(() => {
+      const data = {
+        "time": Math.round(Math.random() * 100)
+      }
+      res.write(`data: ${JSON.stringify(data)}`)
+      res.write("\n\n")
+    }, 1000)
+  })
+  .listen(PORT, () => console.log(`http://localhost:${PORT}`))
